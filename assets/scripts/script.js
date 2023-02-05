8aec9f4ff7f6384e603a24dd53743b095f26acdd
Nai <nailahmukhtar@Nailahs-Air.home>
2023-02-05 21:32:58 +0000

    updated values to take user input and written date values to future forecast cards

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,13 +1,25 @@
 var today = moment().format('YYYY-MM-DD')
 var todayPlusDays = moment().add(1,'days').format('YYYY-MM-DD')
 var apiKey = "d55025fbd3d66e451be8af120b4aa003";
-var city_name = "Birmingham";
-var geocodeURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city_name + "&limit=1&appid=" + apiKey;
+var city_name = "";
 var city_lat;
 var city_lon;
 var futureForecasts = [];
 var fiveDayArray = [];
 
+
+$("#search-button").click(function(event) {
+    fiveDayArray = [];
+    event.preventDefault();
+    city_name = $("#search-input").val();
+    console.log(city_name);
+    var geocodeURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city_name + "&limit=1&appid=" + apiKey;
+    apiCall(geocodeURL);
+});
+
+// console.log(city_name);
+
+function apiCall(geocodeURL) {
 //weather city api call for co-ordinates
 $.ajax({
   url: geocodeURL,
@@ -59,21 +71,21 @@ $.ajax({
         console.log(fiveDayArray);
 
         for (let k = 0; k < fiveDayArray.length; k++) {
-    
+            var the_date = moment(fiveDayArray[k][0].dt_txt).format("DD-MM-YYYY");
             var temp = fiveDayArray[k][0].main.temp - 273.15;
             var wind = fiveDayArray[k][0].wind.speed;
             var humidity = fiveDayArray[k][0].main.humidity;
+            var cardId = $('.card')[k].dataset.day;
+            var cardTitle = $('.card[data-day=' + cardId + '] > .card-body > .card-title');
 
-            console.log(temp,wind,humidity);
+            console.log(cardTitle);
 
-            
-            //Writing values to the page
-            // $("#cityName").text(city_name);
-            // $("#currentTemp").text("Temp: " + currentTemp.toFixed(2));
-            // $("#currentWind").text("Wind: " + currentWind);
-            // $("#currentHumidity").text("Humidity: " + currentHumidity);
-
+            if( cardId == [k]) {
+                cardTitle.text(the_date);
+            }
         }
     });
 
 });
+
+}
